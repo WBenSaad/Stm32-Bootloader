357f5404b5e4dbbc1ec1c8d97c4dd60a70f185e8
Wassim Ben Saad <33790012+WBenSaad@users.noreply.github.com>
2021-10-05 00:10:23 +0200

    Delete script.bat

--- script.bat
+++
@@ -1,6 +0,0 @@
-make
-srec_cat @srec/concat.txt
-srec_cat @srec/Hex_Signed.txt
-srec_cat @srec/unfill.txt
-srec_cat @srec/convert.txt
-openocd -f interface/stlink.cfg -f board/stm32f103c8_blue_pill.cfg -c "program App.bin 0x08000000 reset exit"
